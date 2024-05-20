86ee58bb462939da2d9b2852693af8d08e3aaa89
anubhav-18 <anubhavbindal18@gmail.com>
2024-05-20 17:46:45 +0530

    inital

---
+++ lib/events_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Events_details extends StatefulWidget {
+  const Events_details({super.key});
+
+  @override
+  State<Events_details> createState() => _Events_detailsState();
+}
+
+class _Events_detailsState extends State<Events_details> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
